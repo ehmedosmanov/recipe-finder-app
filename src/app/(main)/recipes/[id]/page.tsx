ea67018c3bd374c3bc82b363756255d3a4381d4f
ehmedosmanov <ehmedosmanov@gmail.com>
2025-04-20 00:58:13 +0400

    fix: fixed params id bug

--- src/app/(main)/recipes/[id]/page.tsx
+++ src/app/(main)/recipes/[id]/page.tsx
@@ -2,11 +2,10 @@ import React, { Suspense } from 'react';
 import Loading from './loading';
 import RecipeSingle from '@/components/RecipeSingle';
 
-type RecipeDetailProps = {
-  params: { id: string };
-};
+type Params = Promise<{ id: string }>;
 
-const RecipeDetail = ({ params }: RecipeDetailProps) => {
+const RecipeDetail = async (props: { params: Params }) => {
+  const params = await props.params;
   return (
     <div className="p-4 md:p-8 bg-gradient-to-b from-amber-50 to-orange-50">
       <div className="max-w-4xl mx-auto">
